058a378cb027fe1dfb973599f29a5a27bd7db6a9
Adam mccool <strongnordwomen@gmail.com>
2022-05-17 15:30:02 +0930

    Project files

---
+++ Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello", this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move back and forth
+        transform.position += 
+            new Vector3(Mathf.Sin(Time.timeSinceLevelLoad * speed) * distance, 0, 0) * Time.deltaTime;
+    }
+}
